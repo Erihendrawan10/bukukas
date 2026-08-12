f70c21d0ea45026af0ba26219c86b76d45a264f5
Erihendrawan10 <kurasintoren@gmail.com>
2026-08-12 15:35:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Ganti URL di bawah ini dengan URL Web App hasil Deploy dari Google Apps Script.
+// Contoh: https://script.google.com/macros/s/AKfycb.../exec
+const API_URL = "https://script.google.com/macros/s/AKfycbzrR8vHo3UfpqeqZnPOhBHr1xkIS_6jdRQrOFKMLIvWPOxz9tECJ3g5e8blfK0YMAem/exec";
